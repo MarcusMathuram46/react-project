b7101244f189360702943f737dff68e4d9667124
MarcusMathuram46 <marcus.mathuram7@gmail.com>
2023-10-17 00:01:45 +0530

    components using div

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-const App = () => {
-  return(
-    <p>Hello World!</p>
-  );
-}
+// function App(){
+//   return <p>Hello World!</p>
+// }
 
+// const App = () =>{
+//   return (
+//     <div>
+//       <h1>App Component</h1>
+//       <p>Hello World!</p>
+//     </div>
+//   )
+  
+// }
+
+const App = () =>{
+  return (
+    <>
+      <h1>App Component</h1>
+      <p>Hello World!</p>
+    </>
+  )
+  
+}
 export default App;

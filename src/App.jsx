e7a18a2dffdeba3934724f72e1b3b6c168bc76ce
MarcusMathuram46 <marcus.mathuram7@gmail.com>
2023-10-17 00:06:25 +0530

    create a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
-// function App(){
-//   return <p>Hello World!</p>
-// }
-
-// const App = () =>{
-//   return (
-//     <div>
-//       <h1>App Component</h1>
-//       <p>Hello World!</p>
-//     </div>
-//   )
-  
-// }
-
-const App = () =>{
-  return (
-    <>
-      <h1>App Component</h1>
-      <p>Hello World!</p>
-    </>
-  )
-  
+function App() {
+  return <p>Hello World</p>
 }
+
 export default App;

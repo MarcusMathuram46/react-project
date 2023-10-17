5bc06110ad7472d1c02626136790e3dafdd380f6
MarcusMathuram46 <marcus.mathuram7@gmail.com>
2023-10-17 20:01:11 +0530

    mini task using useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
-const App = () => {
-  return(
-    <p>Hello World!</p>
-  );
+/*
+  Hooks: 
+    - any function that starts with "use" is called a hook
+    - they are special functions that are available only while React is rendering.
+
+    To create a counter, where the value is increased as a function of time or at the click of a button.
+*/
+
+import React, { useState } from 'react';
+
+function App() {
+
+  const [counter, setCounter] = useState(0);
+
+
+  function handleClickAdd() {
+    setCounter(counter + 1);
+  }
+  function handleClickSub() {
+    setCounter(counter - 1);
+  }
+  function handleClickZero() {
+    setCounter(0);
+  }
+
+  return (
+    <div>
+      <div>
+      <p>{ counter }</p>
+      <button onClick={handleClickAdd}>plus</button>
+      <button onClick={handleClickSub}>minus</button>
+      <button onClick={handleClickZero}>zero</button>
+      </div>
+    
+    </div>
+  )
 }
 
 export default App;
